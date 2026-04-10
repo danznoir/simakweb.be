a371197343bdb359274dadb49d1917ee4b7ad0a7
mohdthoriq <mohdthoriq@gmail.com>
2026-04-10 20:57:55 +0700

    feat : set up cloudinary

---
+++ src/utils/env.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const config = {
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+};
+
+export default config;
